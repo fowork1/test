feeb4172a9e1f997e09ebe2713bef6c9ce31c377
polin <fowork1@yandex.ru>
2020-09-28 01:38:00 +0300

    add model, migration and seeder for realty

--- database/migrations/2020_09_27_220149_create_realties_table.php
+++ database/migrations/2020_09_27_220149_create_realties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRealtiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('realties', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->float('price');
             $table->timestamps();
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->integer('storeys');
+            $table->integer('garages');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('realties');
     }
 }
